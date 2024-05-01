87b4eedafbf140ee9c84e088bf12b6bcf4047484
Muhammet Erol <muhammederol971@gmail.comgit config --global user.email muhammederol971@gmail.com>
2024-05-02 01:25:38 +0200

    added local storafe and finished the redux practice.

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,23 +1,39 @@
 import { useSelector, useDispatch } from "react-redux";
 import TodoItem from "./TodoItem";
 import { clr } from "../redux/toDoSlice";
+import { useEffect, useState } from "react";
 
 const TodoList = () => {
   const toDolist = useSelector((state) => state.toDo.todoList);
   const dispatch = useDispatch();
+  const [localData, setLocalData] = useState(
+    JSON.parse(localStorage.getItem("toDolist")) || []
+  );
 
   const handleClearList = () => {
     dispatch(clr());
   };
 
+  useEffect(() => {
+    localStorage.setItem("toDolist", JSON.stringify(toDolist));
+    setLocalData(toDolist);
+  }, [toDolist]);
+
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem("toDolist"));
+    if (storedData) {
+      setLocalData(storedData);
+    }
+  }, []);
+
   return (
     <div>
       <div>
-        {toDolist.map((todo) => (
+        {localData.map((todo) => (
           <TodoItem key={todo.id} {...todo} />
         ))}
       </div>
-      {toDolist.length ? (
+      {localData.length ? (
         <div className="flex items-center justify-center mt-5">
           <button
             className="p-4 bg-red-500 text-white w-20 rounded "
@@ -26,7 +42,9 @@ const TodoList = () => {
             Clear
           </button>
         </div>
-      ):""}
+      ) : (
+        ""
+      )}
     </div>
   );
 };
