b75da17c94597db57e9bdf2db42801edc532ce52
Muhammet Erol <muhammederol971@gmail.comgit config --global user.email muhammederol971@gmail.com>
2024-05-01 17:06:50 +0200

    adding todo item

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,14 +1,24 @@
 import { useState } from "react"
 import {  useDispatch } from 'react-redux'
-
+import { add } from "../redux/toDoSlice";
 
 const TodoInput = () => {
+  const [text, setText] = useState("")
+  const dispatch = useDispatch()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    dispatch(add(text))
+    setText("")
+  }
+
   return (
-    <form className="w-[100%] flex justify-center gap-4">
+    <form className="w-[100%] flex justify-center gap-4" onSubmit={handleSubmit}>
       <input
         type="text"
         className="p-3 rounded w-1/3 focus:outline-none"
         placeholder="Add Todo..."
+        onChange={(e) => setText(e.target.value)}
       />
       <button type="submit" className="p-4 bg-yellow-500 text-white w-20 rounded ">
         Add
