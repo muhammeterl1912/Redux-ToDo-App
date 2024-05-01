3c0a32c9bb1ea4fa1e3c03000f24b19830a56ed9
Muhammet Erol <muhammederol971@gmail.comgit config --global user.email muhammederol971@gmail.com>
2024-05-01 15:16:34 +0200

    Constructed Todo compenents and configured tailwindcss.

---
+++ src/components/Todo.jsx
@@ -0,0 +1,16 @@
+import TodoInput from "./TodoInput"
+import TodoList from "./TodoList"
+
+const Todo = () => {
+  return (
+    <div>
+  <h2>Todo With Redux</h2>
+
+  <TodoInput />
+      <TodoList />
+
+    </div>
+  )
+}
+
+export default Todo
---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TodoInput = () => {
+  return (
+<form ></form>
+  )
+}
+
+export default TodoInput
